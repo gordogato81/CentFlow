c51fed015aba092047854133e2ada795bcdfbe6a
gordogato81 <51214551+gordogato81@users.noreply.github.com>
2022-06-24 17:06:21 +0200

    Improved layout

--- Frontend/CentFlow/src/app/dialog/dialog.component.ts
+++ Frontend/CentFlow/src/app/dialog/dialog.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DialogData } from '../interfaces';
+import * as L from 'leaflet';
+import * as d3 from 'd3';
+
 
 @Component({
   selector: 'app-dialog',
@@ -7,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) { }
+
+  private map!: L.Map;
 
   ngOnInit(): void {
-  }
+    const mapOptions = {
+      zoom: 4,
+      zoomDelta: 0.5,
+      minZoom: 4,
+      maxZoom: 9,
+    };
 
+    this.map = L.map('dMap', mapOptions).setView([0, 80]); // defaults to world view 
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 9,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.map);
+    L.svg().addTo(this.map);
+    L.canvas().addTo(this.map);
+  }
 }
